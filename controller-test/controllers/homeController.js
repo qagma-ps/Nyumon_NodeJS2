3e7fdf8e67e5eedb8ac2302d78ed7b279f6d049f
qagma-ps <yuichiro.a30@gmail.com>
2020-12-20 03:19:39 +0900

    lesson 15 completed

--- controller-test/controllers/homeController.js
+++ controller-test/controllers/homeController.js
@@ -3,4 +3,25 @@
 exports.respondWithName = (req, res) => {
   let paramsName = req.params.myName;
   res.render("index", {name: paramsName});
-}
+};
+
+var courses = [
+  {
+    title: "Event Driven Cakes",
+    cost: 50
+  },
+  {
+    title: "Asynchronous Artichoke",
+    cost: 25
+  },
+  {
+    title: "Object Oriented Orange Juice",
+    cost: 10
+  }
+];
+
+exports.showCourses = (req, res) => {
+  res.render("courses", {
+    offeredCourses: courses
+  });
+};
--- controller-test/controllers/subscribersController.js
+++ controller-test/controllers/subscribersController.js
@@ -2,14 +2,19 @@
 
 const Subscriber = require("../models/subscriber");
 
-exports.getAllSubscribers = (req, res, next) => {
-  Subscriber.find({}, (error, subscribers) => {
-    if(error) next (error);
-    req.data = subscribers;
-    console.log(req.data);
-    res.render("subscribers", {subscribers: req.data});
-    next();
-  });
+exports.getAllSubscribers = (req, res) => {
+  Subscriber.find({})
+    .exec()
+    .then((subscribers) => {
+      res.render("subscribers", {subscribers: subscribers});
+    })
+    .catch((error) =>{
+      console.log(error.message);
+      return [];
+    })
+    .then(() => {
+      console.log("promise complete");
+    });
 };
 
 exports.getSubscriptionPage = (req, res) => {
@@ -22,8 +27,11 @@ exports.saveSubscriber = (req, res) => {
     email: req.body.email,
     zipCode: req.body.zipCode
   });
-  newSubscriber.save((error, result) => {
-    if (error) res.send(error);
-    res.render("thanks");
-  });
+  newSubscriber.save()
+    .then((result) => {
+      res.render("thanks");
+    })
+    .catch((error) => {
+      if(error) res.send(error);
+    });
 };
